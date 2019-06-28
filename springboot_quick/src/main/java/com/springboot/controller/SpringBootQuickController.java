702e62f99a284efe41877711b5b0590c678a5442
bailongma <942593936@qq.com>
2019-06-28 18:03:40 +0800

    SpringBoot第六次提交

--- springboot_quick/src/main/java/com/springboot/controller/SpringBootQuickController.java
+++ springboot_quick/src/main/java/com/springboot/controller/SpringBootQuickController.java
@@ -9,7 +9,7 @@ public class SpringBootQuickController {
     @RequestMapping("/quick")
     @ResponseBody
     public String quick(){
-        int i= 102;
+        int i= 103;
         return "Hello,Springboot";
     }
 
